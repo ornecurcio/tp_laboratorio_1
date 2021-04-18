5e411b96e930c7102378e1e1ae049da45b8fcc13
ornecurcio <ornecurcio@gmail.com>
2021-04-17 23:00:17 -0300

    describi las funciones

--- Labo1Tp1/src/calculos.h
+++ Labo1Tp1/src/calculos.h
@@ -7,13 +7,22 @@
 
 #ifndef CALCULOS_H_
 #define CALCULOS_H_
-
+// DEVUELVEN != 0 SI HAY ALGUN ERROR, DEVUELVE (0) SI NO HAY ERRORES
 int utn_sumar(int* pResultado, int numUno, int numDos);
+//toma dos parametros, numeros enteros por valor, pasa por referencia el resultado de la suma
 int utn_restar(int* pResultado, int numUno, int numDos);
+//toma dos parametros, numeros enteros por valor, pasa por referencia el resultado de la resta
 int utn_multiplicar(int* pResultado, int numUno, int numDos);
+//toma dos parametros, numeros enteros por valor, pasa por referencia el resultado de la multiplicacion
 int utn_dividir(float* pResultado, int numUno, int numDos);
+////toma dos parametros, numeros enteros por valor, pasa por referencia el resultado de la division, valida que numDos!=0.
 int utn_promediarArray(float* pPromedioResultado, int array[], int cantidadArray);
+//recorre un array de numeros enteros, realiza el promedio del mismo y pasa su resultado por referencia
 void utn_imprimirArray(int listaDeArray[], int cantidadDeArray);
+// recorre un array de numeros enteros, devuelve por consola los valor guardados en el array
 int utn_factorial(int* pResultado, int num);
+//toma por valor un numero entero positivo, realiza el factorial del mismo y pasa el resultado por referencia
 int imprimirArrayResultados(int listaDeArray[], int resultadosArray[], float resultadoDiv);
+//toma por referencia los arrays, muestra por consola los resultados
+
 #endif /* CALCULOS_H_ */
--- Labo1Tp1/src/funcionesUTNyMenu.h
+++ Labo1Tp1/src/funcionesUTNyMenu.h
@@ -15,6 +15,15 @@ int utn_getNumero(int* pResultado,char* mensaje,char* mensajeError,int minimo,in
  * estable cantidad de reintentos de error.
  * */
 int utn_getNumeroConDecimales(float* pResultado,char* mensaje,char* mensajeError,float minimo,float maximo,int reintentos);
-
+/*obtiene un numero flotante, asigno el resultado a un espacio de memoria,
+ * muestra un mensaje de ingreso de dato, un mensaje de error si la validacion esta mal,
+ * valida contra minimo y maximo
+ * estable cantidad de reintentos de error.
+ */
 int utn_getCaracter(char* pResultado,char* mensaje,char* mensajeError,char minimo,char maximo,int reintentos);
+/*obtiene un caracter , asigno el resultado a un espacio de memoria,
+ * muestra un mensaje de ingreso de dato, un mensaje de error si la validacion esta mal,
+ * valida contra minimo y maximo
+ * estable cantidad de reintentos de error.
+ */
 #endif /* FUNCIONESUTNYMENU_H_ */
